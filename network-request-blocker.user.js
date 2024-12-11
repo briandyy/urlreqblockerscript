4c90c9fd14dba55d96ff3215c16b231aa2f9b967
Afif Febriandy <158464353+briandyy@users.noreply.github.com>
2024-12-11 13:38:27 +0700

    Update network-request-blocker.user.js

--- network-request-blocker.user.js
+++ network-request-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Comprehensive Network Request Blocker
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.2.1
 // @description  Block network requests (GET, POST, and others) similar to Chrome DevTools' Network block function
 // @author       Your Name
 // @match        *://*/*
